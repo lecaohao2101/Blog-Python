743c216ca77a2372667c91958e624105778fce02
Your Name <you@example.com>
2023-03-15 13:15:16 +0700

    template and jinja

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 # request cua nguoi dung
 def index(request):
-    response = HttpResponse()
-    response.writelines("<h1>Xin Chao</h1>")
-    response.write("This is app home")
-    return response
+    # response = HttpResponse()
+    # response.writelines("<h1>Xin Chao</h1>")
+    # response.write("This is app home")
+    return render(request, 'pages/home.html')
