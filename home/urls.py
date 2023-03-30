58bf3878daeebc8aeae628a6a0a81fa31cdaddef
Your Name <you@example.com>
2023-03-31 04:52:26 +0700

    create form register

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('contact/', views.contact, name='contact'),
-
+    path('register/', views.register, name='register'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import RegistrationForm
 from django.http import HttpResponse
-
 
 # Create your views here.
 # request cua nguoi dung
@@ -23,3 +24,13 @@ def error(request, exception):
 
 def error(request):
     return render(request, 'pages/error.html')
+
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        #no se goi nhung def tren
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'pages/register.html', {'form': form})
