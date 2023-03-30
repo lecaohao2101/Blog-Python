8f4068a71db91b96bd036032f57f07a843cfb701
Your Name <you@example.com>
2023-03-31 03:14:50 +0700

    upload file

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1tg2$@m294^6c#*ryjb9tiz6hh7i_-thv_u%k#p-d1=+9xpgf5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #luu nhung host vaf domain co the cho server cung cap dich vu
 ALLOWED_HOSTS = ["*"]
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#file tinh
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+#create link to media folder
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
