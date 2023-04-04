1ddb98b18634fed9d03b985b7c6840898c62593f
Your Name <you@example.com>
2023-04-04 17:55:51 +0700

    Generic view

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
-def listblog(request):
-    Data = {'Posts': Post.objects.all().order_by("-date")}
-    return render(request, 'blog/blog.html', Data)
+# def listblog(request):
+#     Data = {'Posts': Post.objects.all().order_by("-date")}
+#     return render(request, 'blog/blog.html', Data)
 
+# class PostListView(ListView):
+#     queryset = Post.objects.all().order_by("-date")
+#     template_name = 'blog/blog.html'
+#     context_object_name = 'Posts'
+#     paginate_by = 10
 
-def post(request, id):
-    post = Post.objects.get(id=id)
-    return render(request, 'blog/post.html', {'post': post})
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = 'blog/post.html'
+# def post(request, id):
+#     post = Post.objects.get(id=id)
+#     return render(request, 'blog/post.html', {'post': post})
